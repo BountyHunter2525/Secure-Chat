d24300c91585c9c7005d517dd23d4c597cd4b97a
BountyHunter2525 <pushkarshah1592@gmail.com>
2026-06-04 01:54:41 +0530

    Setup Supabase backend and database schema

---
+++ src/config/supabase.js
@@ -0,0 +1,18 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://kuqszuplazsdxnuljxzk.supabase.co/rest/v1/';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1cXN6dXBsYXpzZHhudWxqeHprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MDQ4NjUsImV4cCI6MjA5NjA4MDg2NX0.U4MVLeEswzRUOcEDRWwYF-WOIok-IRZqDQZfLRRKq8w';
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  }
+);
